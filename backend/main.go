0a6e336f90f0e1dfbcf0df8dfe84de4e67b37fae
Christopher Tian <mechristophertian@gmail.com>
2023-10-18 20:51:08 -0300

    feat: Starting to work on the backend and adding hot reloading

--- backend/main.go
+++ backend/main.go
@@ -1,14 +1,48 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
 
 func main() {
+	inicializeFiberServer()
+}
+
+func inicializeFiberServer() {
+
 	app := fiber.New()
 
+	app.Use(cors.New(cors.ConfigDefault))
+
 	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Redirect("/api")
+	})
+
+	app.Get("/api", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
 
+	app.Post("api/messages", handleMessages)
+
 	app.Listen(":3000")
 
+}
+
+func handleMessages(c *fiber.Ctx) error {
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return c.Status(503).SendString(err.Error())
+	}
+
+	if data["name"] == "" {
+		data["name"] = "World"
+	}
+
+	data["message"] = "Hello, " + data["name"]
+
+	delete(data, "name")
+
+	return c.JSON(data)
 }
