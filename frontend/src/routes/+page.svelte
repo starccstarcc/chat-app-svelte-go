947d6a6cbc341376a6b61ca46b2cee760e080f51
Christopher Tian <mechristophertian@gmail.com>
2023-10-21 22:14:04 -0300

    refactor: Cleaning boilerplate + Trying out VIM Montions

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,33 +1,13 @@
-<script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
-	import Weather from './Weather.svelte';
-</script>
+<script></script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Svelte + GO + VIM</title>
+	<meta name="description" content="First Svelte APP + Trying VIM Montions" />
 </svelte:head>
 
 <section>
-  <Weather/>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
+  <!-- <Weather/> -->
 
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
 </section>
 
 <style>
@@ -37,25 +17,5 @@
 		justify-content: center;
 		align-items: center;
 		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+  }	
+		</style>
