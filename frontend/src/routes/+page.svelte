78186d29a075bbea41dbfafddfeac2add3083226
Christopher Tian <mechristophertian@gmail.com>
2023-10-21 22:44:49 -0300

    feat: Building a calculator on Svelte

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -5,17 +5,74 @@
 	<meta name="description" content="First Svelte APP + Trying VIM Montions" />
 </svelte:head>
 
-<section>
-  <!-- <Weather/> -->
+<!-- <Weather/> -->
 
-</section>
+<main class="container">
+	<div class="calculator">
+		<div class="results" />
+		<div class="digits">
+			<div class="numbers" />
+			<button>C</button>
+			<button>1</button>
+			<button>2</button>
+			<button>3</button>
+			<button>4</button>
+			<button>5</button>
+			<button>6</button>
+			<button>7</button>
+			<button>8</button>
+			<button>9</button>
+			<button>0</button>
+			<button>.</button>
+		</div>
+		<div class="operations">
+			<button>/</button>
+			<button>X</button>
+			<button>+</button>
+			<button>-</button>
+			<button>=</button>
+		</div>
+	</div>
+</main>
 
 <style>
-	section {
+	.container {
+		width: 100vw;
+		height: 100vh;
 		display: flex;
-		flex-direction: column;
 		justify-content: center;
 		align-items: center;
-		flex: 0.6;
-  }	
-		</style>
+	}
+
+	.calculator {
+		display: flex;
+		background-color: rgb(28, 28, 28);
+		width: 400px;
+		padding: 20;
+		border-radius: 10px;
+	}
+
+	.digits {
+		display: flex;
+	}
+
+	.operations {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.numbers {
+		display: flex;
+		flex-wrap: wrap;
+		width: 200px;
+	}
+
+	.results {
+		display: flex;
+		flex-direction: row-reverse;
+		color: white;
+		font-size: 40px;
+		margin-right: 10px;
+		height: 60px;
+	}
+</style>
