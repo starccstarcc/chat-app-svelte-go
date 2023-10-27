9469a082ceb540ffaa9f51b99b63530e1418279e
Christopher Tian <mechristophertian@gmail.com>
2023-10-26 23:09:44 -0300

    feat: Adding logic to insert a user in the DB
    '

--- backend/pkg/store/postgres/user.go
+++ backend/pkg/store/postgres/user.go
@@ -1,9 +1,31 @@
 package postgres
 
-import "github.com/aletomasella/svelte-go-chat/pkg/domain"
+import (
+	"github.com/aletomasella/svelte-go-chat/pkg/common"
+	"github.com/aletomasella/svelte-go-chat/pkg/domain"
+)
+
+// SQL queries
+
+const (
+	createUserQuery = `INSERT INTO users (email, password) VALUES ($1, $2) RETURNING id`
+)
 
 func (q *postgresStore) CreateUser(user *domain.User) (*domain.User, error) {
-	user.ID = 1
+
+	hashedPassword, err := common.PasswordHash(user.Password)
+
+	if err != nil {
+		return nil, err
+	}
+
+	user.Password = hashedPassword
+
+	err = q.db.QueryRow(createUserQuery, user.Email, user.Password).Scan(&user.ID)
+
+	if err != nil {
+		return nil, err
+	}
 
 	return user, nil
 }
